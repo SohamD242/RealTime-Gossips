3cc506cd1afb7299a5600c82dcd55a9501df1779
Soham <sohamdeshpande242@gmail.com>
2024-10-14 21:29:41 +0530

    Added feature for online users

--- a_rtchat/consumers.py
+++ a_rtchat/consumers.py
@@ -16,13 +16,25 @@ class ChatroomConsumer(WebsocketConsumer):
            self.chatroom_name,
            self.channel_name
         )
+        
+        #adding feature to add & update online users
+        if self.user not in self.chatroom.users_online.all():
+            self.chatroom.users_online.add(self.user)
+            self.update_online_count()
+            
+        
         self.accept()
     
     def disconnect(self, close_code):
         async_to_sync(self.channel_layer.group_discard)(
             self.chatroom_name,
             self.channel_name
-        )    
+        )
+        
+        # removing and updating online users
+        if self.user in self.chatroom.users_online.all():
+            self.chatroom.users_online.remove(self.user)
+            self.update_online_count()
         
     
     def receive(self, text_data):
@@ -52,4 +64,18 @@ class ChatroomConsumer(WebsocketConsumer):
             'user': self.user,
         }
         html = render_to_string("a_rtchat/partials/chat_messages_p.html",context=context)
+        self.send(text_data=html)
+        
+    def update_online_count(self):
+        online_count = self.chatroom.users_online.count()-1
+        
+        event = {
+            'type': 'online_count_handler',
+            'online_count': online_count
+        }
+        async_to_sync(self.channel_layer.group_send)(self.chatroom_name, event)
+        
+    def online_count_handler(self, event):
+        online_count = event['online_count']
+        html = render_to_string("a_rtchat/partials/online_count.html",{'online_count': online_count})
         self.send(text_data=html)
