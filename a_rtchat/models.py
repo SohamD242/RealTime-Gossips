cddb59d49377845dc4404b0fea3d114f56d9449d
Soham <sohamdeshpande242@gmail.com>
2024-11-19 16:11:47 +0530

    added feature for sending files

--- a_rtchat/models.py
+++ a_rtchat/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 import shortuuid
+import os
+from PIL import Image
 
 class ChatGroup(models.Model):
     group_name = models.CharField(max_length=100, unique=True, default=shortuuid.uuid)
@@ -14,13 +16,33 @@ class ChatGroup(models.Model):
         return self.group_name
 
 class GroupMessage(models.Model):
-    group = models.ForeignKey(ChatGroup, on_delete=models.CASCADE, related_name='chat_messages')
+    group = models.ForeignKey(ChatGroup, related_name='chat_messages', on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    body = models.CharField(max_length=300)
-    created = models.DateTimeField(auto_now_add=True )
-
+    body = models.CharField(max_length=300, blank=True, null=True)
+    file = models.FileField(upload_to='files/', blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    @property
+    def filename(self):
+        if self.file:
+            return os.path.basename(self.file.name)
+        else:
+            return None
+    
     def __str__(self):
-        return f'{self.author.username} : {self.body}'
+        if self.body:
+            return f'{self.author.username} : {self.body}'
+        elif self.file:
+            return f'{self.author.username} : {self.filename}'
     
     class Meta:
         ordering = ['-created']
+        
+    @property    
+    def is_image(self):
+        try:
+            image = Image.open(self.file) 
+            image.verify()
+            return True 
+        except:
+            return False
